5faa55e7937bb7d52dd36515009336b6a7796a8b
Jason Brown <stopthatastronaut@users.noreply.github.com>
2021-06-22 21:40:46 +1000

    finish the quickstart, fool around w/templates a bit

--- quickstart/products/views.py
+++ quickstart/products/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World!</h1>")
+def home_view(request, *args, **kwargs):
+    today = datetime.now()
+    template = loader.get_template("home.html")
+    context = {
+        'todays_date': today}
+
+    return HttpResponse(template.render(context, request))
